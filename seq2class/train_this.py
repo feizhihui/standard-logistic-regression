80061070f33ed310d1f9d11e744276c1112dfdf4
feizhihui <helloworld@csu.edu.cn>
2017-11-20 16:23:26 +0800

    add seq2class package

--- seq2class/train_this.py
+++ seq2class/train_this.py
@@ -17,7 +17,7 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     sess.run(tf.local_variables_initializer())
     for epoch in range(epoch_num):
-        print('epoch - ', str(epoch + 1))
+        print('========== epoch - ', str(epoch + 1), "===================")
         master.shuffle()
         for step, index in enumerate(range(0, master.datasize, batch_size)):
             batch_xs = master.train_x[index:index + batch_size]
@@ -31,7 +31,7 @@ with tf.Session() as sess:
                 print("cost function: %.3f, accuracy: %.3f, auc: %.3f" % (batch_cost, batch_accuracy, auc))
                 print("Precision %.6f" % metrics.precision_score(batch_ys, y_pred))
                 print("Recall %.6f" % metrics.recall_score(batch_ys, y_pred))
-                print("f1_score %.6f" % metrics.f1_score(batch_ys, y_pred))
+                print("F1-score %.6f" % metrics.f1_score(batch_ys, y_pred))
 
     # store
     saver = tf.train.Saver()
